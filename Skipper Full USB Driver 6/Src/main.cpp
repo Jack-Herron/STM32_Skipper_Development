c50e592ca478fe8f2601340d742eae9551558377
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-06-21 22:26:40 -0700

    Attach to STM32CubeIDE

---
+++ Skipper Full USB Driver 6/Src/main.cpp
@@ -0,0 +1,48 @@
+
+/*
+ * date [date]
+ * Project name - [your project name]
+ * Created by: [your name]
+ *
+ * description[description]
+ */
+
+#include <stdint.h>					// include standard c functions
+#include <stdio.h>
+#include <stm32f4xx.h>				// include MCU specific definitions
+#include <Skipper.h>				// include skipper library
+#include <cmath>					// include CMath
+
+void GPIO_init(void){
+	RCC->AHB1ENR |= (1<<3); 		// enable GPIOD clock
+	RCC->AHB1ENR |= (1<<2); 		// enable GPIOC clock
+	RCC->AHB1ENR |= (1<<0); 		// enable GPIOA clock
+	//Enable on board LED
+	GPIOD->MODER |= (1<<8); 		// set PD4 to output
+	GPIOD->ODR &= ~(1<<4);			// set PD4 HIGH
+
+	GPIOA->MODER |= (1<<18);
+	GPIOA->ODR |= (1<<9);
+
+	GPIOA->MODER |= (1<<8); 		// set PA4 to output
+	GPIOA->ODR &= ~(1<<4);			// set PA4 HIGH
+
+	GPIOA->MODER |= (1<<6); 		// set PA3 to output
+	GPIOA->ODR &= ~(1<<3);			// set PA3 HIGH
+
+	GPIOA->MODER |= (1<<4); 		// set PA2 to output
+	GPIOA->ODR &= ~(1<<2);			// set PA2 HIGH
+
+}
+
+int main(void) {
+	clock.setParams(8,72,2,3,1,2,1);				// set the clock parameters
+	clock.init();									// initiate the clock
+	GPIO_init();									//initiate the GPIO to be used in this program.
+
+	for(;;)
+	{
+
+	}
+}
+
---
+++ Skipper Full USB Driver 6/USB_HOST/Core/Src/USB_Host_Hub.cpp
@@ -0,0 +1,10 @@
+/*
+ * USB_Host_Hub.cpp
+ *
+ *  Created on: Jun 21, 2024
+ *      Author: Jack Herron
+ */
+
+
+
+
