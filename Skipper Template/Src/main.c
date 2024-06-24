0c364abaea45105f8b2abd334bbedca5f82bee80
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-06-23 22:20:05 -0700

    re-factored, fixed comments, included stdlib.h.

--- Skipper Template/Src/main.c
+++ Skipper Template/Src/main.c
@@ -7,23 +7,27 @@
  * Description		[description]
  */
 
+#include <stdlib.h>  									// Include C standard library
 #include <stdint.h>										// Include C library for fixed-width integer types
-#include <stm32f4xx.h>									// include STM32F4 specific definitions
+#include <stm32f4xx.h>									// Include STM32F4 specific definitions
 
-void GPIO_init(void){
-	RCC->AHB1ENR 	|= (RCC_AHB1ENR_GPIODEN); 			// enable GPIOD clock
+void GPIO_init(void)
+{
+	RCC->AHB1ENR 	|= (RCC_AHB1ENR_GPIODEN); 			// Enable GPIOD clock
 
 	//Enable on board LED
-	GPIOD -> MODER 	|= (GPIO_MODER_MODER4_0); 	// set PD4 to output
-	GPIOD -> ODR 	|= (GPIO_ODR_ODR_4);				// set PD4 HIGH
+	GPIOD -> MODER 	|= (GPIO_MODER_MODER4_0); 			// Set PD4 to output
+	GPIOD -> ODR 	|= (GPIO_ODR_ODR_4);				// Set PD4 HIGH
 }
 
-int main(void) {
-	GPIO_init();										// initiate the GPIO to be used in this program.
+int main(void)
+{
+	GPIO_init();										// Initiate the GPIO's to be used in this program
 
 	for(;;)
 	{
 
 	}
-}
 
+	return(EXIT_SUCCESS);
+}
