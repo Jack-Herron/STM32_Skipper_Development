761e6cfd7c1901d77d1596912b2f1c83e3ba2331
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-06-24 01:06:02 -0700

    Set up AHB and APB value translation functions.

--- Skipper Template/Drivers/Skipper Drivers/Inc/Skipper_Clock.h
+++ Skipper Template/Drivers/Skipper Drivers/Inc/Skipper_Clock.h
@@ -8,7 +8,37 @@
 #ifndef DRIVERS_SKIPPER_DRIVERS_INC_SKIPPER_CLOCK_H_
 #define DRIVERS_SKIPPER_DRIVERS_INC_SKIPPER_CLOCK_H_
 
+#define Skipper_Clock___APB_DIV_1			1
+#define Skipper_Clock___APB_DIV_2			2
+#define Skipper_Clock___APB_DIV_4			4
+#define Skipper_Clock___APB_DIV_8			8
+#define Skipper_Clock___APB_DIV_16			16
 
+#define Skipper_Clock___APB_DIV_1_VALUE		0
+#define Skipper_Clock___APB_DIV_2_VALUE		4
+#define Skipper_Clock___APB_DIV_4_VALUE		5
+#define Skipper_Clock___APB_DIV_8_VALUE		6
+#define Skipper_Clock___APB_DIV_16_VALUE	7
+
+#define Skipper_Clock___AHB_DIV_1			1
+#define Skipper_Clock___AHB_DIV_2			2
+#define Skipper_Clock___AHB_DIV_4			4
+#define Skipper_Clock___AHB_DIV_8			8
+#define Skipper_Clock___AHB_DIV_16			16
+#define Skipper_Clock___AHB_DIV_64			64
+#define Skipper_Clock___AHB_DIV_128			128
+#define Skipper_Clock___AHB_DIV_256			256
+#define Skipper_Clock___AHB_DIV_512			512
+
+#define Skipper_Clock___AHB_DIV_1_VALUE		0
+#define Skipper_Clock___AHB_DIV_2_VALUE		8
+#define Skipper_Clock___AHB_DIV_4_VALUE		9
+#define Skipper_Clock___AHB_DIV_8_VALUE		10
+#define Skipper_Clock___AHB_DIV_16_VALUE	11
+#define Skipper_Clock___AHB_DIV_64_VALUE	12
+#define Skipper_Clock___AHB_DIV_128_VALUE	13
+#define Skipper_Clock___AHB_DIV_256_VALUE	14
+#define Skipper_Clock___AHB_DIV_512_VALUE	15
 
 
 
--- Skipper Template/Drivers/Skipper Drivers/Src/Skipper_Clock.c
+++ Skipper Template/Drivers/Skipper Drivers/Src/Skipper_Clock.c
@@ -5,4 +5,93 @@
  *      Author: Jack Herron
  */
 
+#include <stdlib.h>  									// Include C standard library
+#include <stdint.h>										// Include C library for fixed-width integer types
+#include <stm32f4xx.h>									// Include STM32F4 specific definitions
+#include <assert.h>										// Include assert
 #include "../Inc/Skipper_Clock.h"
+
+uint8_t Skipper_Clock___Get_APB_Value_From_Divisor(uint8_t Divisor)
+{
+	uint8_t APB_Value;
+
+	switch(Divisor)
+	{
+		case Skipper_Clock___APB_DIV_1:
+			APB_Value = Skipper_Clock___APB_DIV_1_Value;
+			break;
+
+		case Skipper_Clock___APB_DIV_2:
+			APB_Value = Skipper_Clock___APB_DIV_2_Value;
+			break;
+
+		case Skipper_Clock___APB_DIV_4:
+			APB_Value = Skipper_Clock___APB_DIV_4_Value;
+			break;
+
+		case Skipper_Clock___APB_DIV_8:
+			APB_Value = Skipper_Clock___APB_DIV_8_Value;
+			break;
+
+		case Skipper_Clock___APB_DIV_16:
+			APB_Value = Skipper_Clock___APB_DIV_16_Value;
+			break;
+
+		default:
+			assert(1);													// APB value invalid
+			break;
+	}
+
+	return(APB_Value);
+}
+
+uint8_t Skipper_Clock___Get_AHB_Value_From_Divisor(uint16_t Divisor)
+{
+	uint8_t AHB_Value;
+
+	switch(Divisor)
+	{
+		case Skipper_Clock___AHB_DIV_1:
+			AHB_Value = Skipper_Clock___AHB_DIV_1_VALUE;
+			break;
+
+		case Skipper_Clock___AHB_DIV_2:
+			AHB_Value = Skipper_Clock___AHB_DIV_2_VALUE;
+			break;
+
+		case Skipper_Clock___AHB_DIV_4:
+			AHB_Value = Skipper_Clock___AHB_DIV_4_VALUE;
+			break;
+
+		case Skipper_Clock___AHB_DIV_8:
+			AHB_Value = Skipper_Clock___AHB_DIV_8_VALUE;
+			break;
+
+		case Skipper_Clock___AHB_DIV_16:
+			AHB_Value = Skipper_Clock___AHB_DIV_16_VALUE;
+			break;
+
+		case Skipper_Clock___AHB_DIV_64:
+			AHB_Value = Skipper_Clock___AHB_DIV_64_VALUE;
+			break;
+
+		case Skipper_Clock___AHB_DIV_128:
+			AHB_Value = Skipper_Clock___AHB_DIV_128_VALUE;
+			break;
+
+		case Skipper_Clock___AHB_DIV_256:
+			AHB_Value = Skipper_Clock___AHB_DIV_256_VALUE;
+			break;
+
+		case Skipper_Clock___AHB_DIV_512:
+			AHB_Value = Skipper_Clock___AHB_DIV_512_VALUE;
+			break;
+
+		default:
+			assert(1);													// AHB value invalid
+			break;
+	}
+
+	return(AHB_Value);
+}
+
