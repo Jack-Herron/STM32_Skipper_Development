d317795665c871563cba5877c3065cc306956d00
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-06-26 13:29:10 -0700

    Add relevant includes.

--- USB Device And Host Development/USB_CORE/Src/USB_LL_Hardware.c
+++ USB Device And Host Development/USB_CORE/Src/USB_LL_Hardware.c
@@ -5,6 +5,8 @@
  *      Author: Jack Herron
  */
 
+#include <stdint.h>					// Include C library for fixed-width integer types
+#include <stm32f4xx.h>				// include MCU specific definitions
 #include "../Inc/USB_LL_Hardware.h"
 
 
--- USB Device And Host Development/USB_CORE/Src/USB_LL_Interrupts.c
+++ USB Device And Host Development/USB_CORE/Src/USB_LL_Interrupts.c
@@ -5,4 +5,6 @@
  *      Author: Jack Herron
  */
 
+#include <stdint.h>					// Include C library for fixed-width integer types
+#include <stm32f4xx.h>				// include MCU specific definitions
 #include "../Inc/USB_LL_Interrupts.h"
