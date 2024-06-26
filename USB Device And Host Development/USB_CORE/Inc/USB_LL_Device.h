12e5bd2c1d9d82efe3e049281978fde1f965b126
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-06-26 16:48:16 -0700

    add relevant #includes

--- USB Device And Host Development/USB_CORE/Inc/USB_LL_Device.h
+++ USB Device And Host Development/USB_CORE/Inc/USB_LL_Device.h
@@ -8,8 +8,13 @@
 #ifndef INC_USB_LL_DEVICE_H_
 #define INC_USB_LL_DEVICE_H_
 
-
-
+#include <stdlib.h>						// include c standard library
+#include <stdint.h>						// Include C library for fixed-width integer types
+#include <stm32f4xx.h>					// include MCU specific definitions
+#include <Skipper_Clock.h>
+#include "../Inc/USB_LL_Interrupts.h"
+#include "../Inc/USB_LL_Hardware.h"
+#include "../Inc/USB_LL_Device.h"
 
 
 #endif /* INC_USB_LL_DEVICE_H_ */
--- USB Device And Host Development/USB_CORE/Inc/USB_LL_Host.h
+++ USB Device And Host Development/USB_CORE/Inc/USB_LL_Host.h
@@ -8,8 +8,13 @@
 #ifndef INC_USB_LL_HOST_H_
 #define INC_USB_LL_HOST_H_
 
-
-
+#include <stdlib.h>						// include c standard library
+#include <stdint.h>						// Include C library for fixed-width integer types
+#include <stm32f4xx.h>					// include MCU specific definitions
+#include <Skipper_Clock.h>
+#include "../Inc/USB_LL_Interrupts.h"
+#include "../Inc/USB_LL_Hardware.h"
+#include "../Inc/USB_LL_Host.h"
 
 
 #endif /* INC_USB_LL_HOST_H_ */
