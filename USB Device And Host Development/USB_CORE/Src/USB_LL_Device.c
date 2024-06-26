95b07a340056ea0e17b28c59c6267be457fddc20
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-06-26 16:52:16 -0700

    fixed mistake that added all #includes to the header instead of source

--- USB Device And Host Development/USB_CORE/Src/USB_LL_Device.c
+++ USB Device And Host Development/USB_CORE/Src/USB_LL_Device.c
@@ -5,4 +5,10 @@
  *      Author: Jack Herron
  */
 
-
+#include <stdlib.h>						// include c standard library
+#include <stdint.h>						// Include C library for fixed-width integer types
+#include <stm32f4xx.h>					// include MCU specific definitions
+#include <Skipper_Clock.h>
+#include "../Inc/USB_LL_Interrupts.h"
+#include "../Inc/USB_LL_Hardware.h"
+#include "../Inc/USB_LL_Device.h"
