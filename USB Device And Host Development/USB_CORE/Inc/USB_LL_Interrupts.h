8d5a4f2ced49ce12b224babb810b515c828a8029
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-06-26 23:24:49 -0700

    Added host port interrupt handler and host channel interrupt handler.

--- USB Device And Host Development/USB_CORE/Inc/USB_LL_Interrupts.h
+++ USB Device And Host Development/USB_CORE/Inc/USB_LL_Interrupts.h
@@ -10,10 +10,11 @@
 
 #define USB_LL_Interrupts___WORD_32_MSB								0x1f
 #define USB_LL_Interrupts___GLOBAL_INTERRUPTS_MASK					0xa33c7c98
+#define USB_LL_Interrupts___HOST_PORT_INTERRUPTS_MASK				0x000a
 #define USB_LL_Interrupts___IN_ENDPOINT_INTERRUPTS_MASK				0x000b
 #define USB_LL_Interrupts___OUT_ENDPOINT_INTERRUPTS_MASK			0x000b
 #define USB_LL_Interrupts___CHANNEL_INTERRUPTS_MASK					0x029b
-
+#define USB_LL_Interrupts___HPRT_RC_W1_BITS							0x2e
 typedef enum {
 	USB_Proccess_Status___IDLE,
 	USB_Proccess_Status___TRANSFER_COMPLETE,
