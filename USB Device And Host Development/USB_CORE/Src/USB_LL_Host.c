51e1c675fd341a21c4519c435b5d4db02cfd35c1
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-06-26 17:11:05 -0700

    Fixed function names, moved #defines over from USB_LL_Hardware.h.

--- USB Device And Host Development/USB_CORE/Src/USB_LL_Host.c
+++ USB Device And Host Development/USB_CORE/Src/USB_LL_Host.c
@@ -15,7 +15,7 @@
 
 // ---------------------------- USB HOST CONTROL SECTION -----------------------------
 
-void USB_LL_Hardware___Host_Set_FIFO_Size(uint8_t port_Number, uint32_t RX_FIFO_Depth, uint32_t non_Periodic_TX_FIFO_Depth, uint32_t periodic_TX_FIFO_Depth)
+void USB_LL_Host___Set_FIFO_Size(uint8_t port_Number, uint32_t RX_FIFO_Depth, uint32_t non_Periodic_TX_FIFO_Depth, uint32_t periodic_TX_FIFO_Depth)
 {
 	USB_OTG_GlobalTypeDef* USB = USB_LL_Hardware___Get_USB(port_Number);
 	uint32_t Start_Address = 0;
@@ -31,17 +31,17 @@ void USB_LL_Hardware___Host_Set_FIFO_Size(uint8_t port_Number, uint32_t RX_FIFO_
 	USB -> HPTXFSIZ = ((periodic_TX_FIFO_Depth << USB_OTG_HPTXFSIZ_PTXFD_Pos) | (Start_Address << USB_OTG_HPTXFSIZ_PTXSA_Pos));
 }
 
-uint32_t USB_LL_Hardware___Host_Get_FIFO_Space_Available(uint8_t port_Number, uint8_t FIFO_Type)
+uint32_t USB_LL_Host___Get_FIFO_Space_Available(uint8_t port_Number, uint8_t FIFO_Type)
 {
 	switch(FIFO_Type)
 	{
-	case USB_LL_Hardware___NON_PERIODIC_TX_FIFO:
+	case USB_LL_Host___NON_PERIODIC_TX_FIFO:
 	{
 		USB_OTG_GlobalTypeDef* USB = USB_LL_Hardware___Get_USB(port_Number);
 		return(USB_LL_Hardware___GET_BIT_SEGMENT(USB -> HNPTXSTS, USB_OTG_GNPTXSTS_NPTXFSAV_Msk, USB_OTG_GNPTXSTS_NPTXFSAV_Pos));
 		break;
 	}
-	case USB_LL_Hardware___PERIODIC_TX_FIFO:
+	case USB_LL_Host___PERIODIC_TX_FIFO:
 	{
 		USB_OTG_HostTypeDef* USB_Host = USB_LL_Hardware___Get_USB_Host(port_Number);
 		return(USB_LL_Hardware___GET_BIT_SEGMENT(USB_Host -> HPTXSTS, USB_OTG_HPTXSTS_PTXFSAVL_Msk, USB_OTG_HPTXSTS_PTXFSAVL_Pos));
@@ -51,7 +51,7 @@ uint32_t USB_LL_Hardware___Host_Get_FIFO_Space_Available(uint8_t port_Number, ui
 	return(0);
 }
 
-uint16_t USB_LL_Hardware___Host_Get_Frame_Number(uint8_t port_Number)
+uint16_t USB_LL_Host___Get_Frame_Number(uint8_t port_Number)
 {
 	USB_OTG_HostTypeDef* USB_Host = USB_LL_Hardware___Get_USB_Host(port_Number);
 	return(USB_Host->HFNUM & USB_OTG_HFNUM_FRNUM_Msk);
@@ -61,32 +61,32 @@ uint16_t USB_LL_Hardware___Host_Get_Frame_Number(uint8_t port_Number)
 
 // ---------------------------- USB CHANNEL_CONTROL_SECTION --------------------------
 
-uint32_t* USB_LL_Hardware___Channel_Get_Fifo_Pointer(uint8_t port_Number, uint8_t channel_Number)
+uint32_t* USB_LL_Host___Channel_Get_Fifo_Pointer(uint8_t port_Number, uint8_t channel_Number)
 {
 	uint32_t USB_offset = USB_LL_Hardware___Get_USB_BASE(port_Number);
 	return((uint32_t*)(USB_offset + USB_OTG_FIFO_BASE + (USB_OTG_FIFO_SIZE * channel_Number)));
 }
 
-void USB_LLHardware___Channel_Halt(uint8_t port_Number, uint8_t channel_Number)
+void USB_LL_Host___Channel_Halt(uint8_t port_Number, uint8_t channel_Number)
 {
 	USB_OTG_HostChannelTypeDef* USB_Host_Channel = USB_LL_Hardware___Get_USB_Host_Channel(port_Number, channel_Number);
 	USB_Host_Channel -> HCCHAR |= (USB_OTG_HCCHAR_CHDIS | USB_OTG_HCCHAR_CHENA);
 }
 
-void USB_LL_Hardware___Channel_Halt_And_Wait(uint8_t port_Number, uint8_t channel_Number)
+void USB_LL_Host___Channel_Halt_And_Wait(uint8_t port_Number, uint8_t channel_Number)
 {
 	USB_OTG_HostChannelTypeDef* USB_Host_Channel = USB_LL_Hardware___Get_USB_Host_Channel(port_Number, channel_Number);
 	USB_Host_Channel -> HCCHAR |= (USB_OTG_HCCHAR_CHDIS);
 	while(USB_Host_Channel -> HCCHAR & USB_OTG_HCCHAR_CHENA);
 }
 
-uint8_t USB_LL_Hardware___Channel_Get_Current_PID(uint8_t port_Number, uint8_t channel_Number)
+uint8_t USB_LL_Host___Channel_Get_Current_PID(uint8_t port_Number, uint8_t channel_Number)
 {
 	USB_OTG_HostChannelTypeDef* USB_Host_Channel = USB_LL_Hardware___Get_USB_Host_Channel(port_Number, channel_Number);
 	return(USB_LL_Hardware___GET_BIT_SEGMENT(USB_Host_Channel -> HCTSIZ, USB_OTG_HCTSIZ_DPID_Msk, USB_OTG_HCTSIZ_DPID_Pos));
 }
 
-void USB_LL_Hardware___Channel_Set_Interrupts(uint8_t port_Number,uint8_t channel_Number)
+void USB_LL_Host___Channel_Set_Interrupts(uint8_t port_Number,uint8_t channel_Number)
 {
 	USB_OTG_HostChannelTypeDef* USB_Host_Channel 	= USB_LL_Hardware___Get_USB_Host_Channel(port_Number, channel_Number);
 	USB_OTG_HostTypeDef*		USB_Host 			= USB_LL_Hardware___Get_USB_Host(port_Number);
@@ -94,13 +94,13 @@ void USB_LL_Hardware___Channel_Set_Interrupts(uint8_t port_Number,uint8_t channe
 	USB_Host_Channel 	-> HCINTMSK  = USB_LL_Interrupts___CHANNEL_INTERRUPTS_MASK;
 }
 
-void USB_LL_Hardware___Channel_Set_Characteristics(uint8_t port_Number, uint8_t channel_Number, uint32_t channel_Characteristics)
+void USB_LL_Host___Channel_Set_Characteristics(uint8_t port_Number, uint8_t channel_Number, uint32_t channel_Characteristics)
 {
 	USB_OTG_HostChannelTypeDef* USB_Host_Channel = USB_LL_Hardware___Get_USB_Host_Channel(port_Number, channel_Number);
 	USB_Host_Channel -> HCCHAR = channel_Characteristics;
 }
 
-void USB_LL_Hardware___Channel_Load_HCTSIZ(uint8_t port_Number, uint8_t channel_Number, uint32_t transfer_Size_In_Bytes, uint32_t packet_Count, uint8_t packet_ID)
+void USB_LL_Host___Channel_Load_HCTSIZ(uint8_t port_Number, uint8_t channel_Number, uint32_t transfer_Size_In_Bytes, uint32_t packet_Count, uint8_t packet_ID)
 {
 	USB_OTG_HostChannelTypeDef* USB_Host_Channel = USB_LL_Hardware___Get_USB_Host_Channel(port_Number, channel_Number);
 	USB_Host_Channel -> HCTSIZ = (transfer_Size_In_Bytes << USB_OTG_HCTSIZ_XFRSIZ_Pos | packet_Count << USB_OTG_HCTSIZ_PKTCNT_Pos | packet_ID << USB_OTG_HCTSIZ_DPID_Pos);
