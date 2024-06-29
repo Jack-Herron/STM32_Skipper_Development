79a1ae6fe7cc3315b0cc7b46d1c9150e4ae30a41
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-06-28 20:36:21 -0700

    added #includes

--- USB Device And Host Development/USB_HOST/Core/Src/USB_Host.c
+++ USB Device And Host Development/USB_HOST/Core/Src/USB_Host.c
@@ -5,3 +5,11 @@
  *      Author: Jack Herron
  */
 
+#include <stdlib.h>						// include c standard library
+#include <stdint.h>						// Include C library for fixed-width integer types
+#include <stm32f4xx.h>					// include MCU specific definitions
+#include <Skipper_Clock.h>
+#include <USB_LL_Hardware.h>
+#include <USB_LL_Host.h>
+#include "../Inc/USB_Host.h"
+
