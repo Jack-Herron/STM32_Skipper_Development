f3dea888a0333a6beeeeaa0309470a00555447c0
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-06-28 20:36:09 -0700

    Added USB_Host_Config header file.

---
+++ USB Device And Host Development/USB_HOST/Core/Inc/USB_Host_Config.h
@@ -0,0 +1,15 @@
+/*
+ * USB_Host_Config.h
+ *
+ *  Created on: Jun 28, 2024
+ *      Author: Jack Herron
+ */
+
+#ifndef CORE_INC_USB_HOST_CONFIG_H_
+#define CORE_INC_USB_HOST_CONFIG_H_
+
+
+
+
+
+#endif /* CORE_INC_USB_HOST_CONFIG_H_ */
