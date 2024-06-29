fd0983001be7f98a2ff6d0f9c52c99d3e217ebee
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-06-28 20:34:49 -0700

    added USB_Device_Manager source and header files.

---
+++ USB Device And Host Development/USB_HOST/Core/Inc/USB_Host_Device_Manager.h
@@ -0,0 +1,15 @@
+/*
+ * USB_Host_Device_Manager.h
+ *
+ *  Created on: Jun 28, 2024
+ *      Author: Jack Herron
+ */
+
+#ifndef CORE_INC_USB_HOST_DEVICE_MANAGER_H_
+#define CORE_INC_USB_HOST_DEVICE_MANAGER_H_
+
+
+
+
+
+#endif /* CORE_INC_USB_HOST_DEVICE_MANAGER_H_ */
---
+++ USB Device And Host Development/USB_HOST/Core/Src/USB_Host_Device_Manager.c
@@ -0,0 +1,8 @@
+/*
+ * USB_Host_Device_Manager.c
+ *
+ *  Created on: Jun 28, 2024
+ *      Author: Jack Herron
+ */
+
+
