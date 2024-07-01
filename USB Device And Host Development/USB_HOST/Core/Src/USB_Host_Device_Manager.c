975951a82184d5b56b28feae726e1b9080795d6c
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-06-30 20:52:56 -0700

    fixed #includes.

--- USB Device And Host Development/USB_HOST/Core/Src/USB_Host_Device_Manager.c
+++ USB Device And Host Development/USB_HOST/Core/Src/USB_Host_Device_Manager.c
@@ -8,5 +8,8 @@
 #include <stdlib.h>						// include c standard library
 #include <stdint.h>						// Include C library for fixed-width integer types
 #include <stm32f4xx.h>					// include MCU specific definitions
+#include "../../Config/USB_Host_Config.h"
+#include "../Inc/USB_Host.h"
 #include "../Inc/USB_Host_Device_Manager.h"
+
 
