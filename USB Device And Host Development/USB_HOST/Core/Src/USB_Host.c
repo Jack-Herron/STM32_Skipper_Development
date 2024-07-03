2b999d74a952e08ce2ab19d55e5262092b2b8a72
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-07-03 00:44:38 -0700

    added functions for USB_Host_Proccess() function.

--- USB Device And Host Development/USB_HOST/Core/Src/USB_Host.c
+++ USB Device And Host Development/USB_HOST/Core/Src/USB_Host.c
@@ -9,6 +9,51 @@
 #include <stdint.h>						// Include C library for fixed-width integer types
 #include <stm32f4xx.h>					// include MCU specific definitions
 #include <Skipper_Clock.h>
+
 #include <USB_LL_Hardware.h>
 #include <USB_LL_Host.h>
+#include <USB_LL_Interrupts_Host.h>
+#include "../Inc/USB_Host_Device_Manager.h"
 #include "../Inc/USB_Host.h"
+
+void USB_Host___Init(uint8_t port_Number)
+{
+	USB_LL_Hardware___GPIO_Init(port_Number);
+	USB_LL_Hardware___Init(port_Number, USB_LL_Hardware___HOST_MODE);
+}
+
+void USB_Host___Proccess_Host_Interrupts(uint8_t port_Number)
+{
+	if(USB_LL_Interrupts_Host___Is_Root_Device_Connection_Status_Change(port_Number))
+	{
+		if(USB_LL_Interrupts_Host___Is_Root_Device_Connected(port_Number))
+		{
+
+		}
+		else
+		{
+
+		}
+	}
+}
+
+void USB_Host___Proccess_Device_Manager_Status(uint8_t port_Number)
+{
+	USB_Host_Device_Manager___Port_Status_TypeDef port_Status = USB_Host_Device_Manager___Get_Port_Status(port_Number);
+
+	if(port_Status.is_New_Device_Connected)
+	{
+
+	}
+
+	if(port_Status.is_New_Device_Enumerated)
+	{
+
+	}
+}
+
+void USB_Host___Proccess(uint8_t port_Number)
+{
+	USB_Host___Proccess_Host_Interrupts(port_Number);
+	USB_Host___Proccess_Device_Manager_Status(port_Number);
+}
