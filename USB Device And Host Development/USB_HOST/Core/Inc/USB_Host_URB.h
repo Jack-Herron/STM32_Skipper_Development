0ddac529507f38413db36e7b6fdec18ec1d75174
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-07-08 15:08:41 -0700

    Added source and header file to contain URB allocation and control.

---
+++ USB Device And Host Development/USB_HOST/Core/Inc/USB_Host_URB.h
@@ -0,0 +1,15 @@
+/*
+ * USB_Host_URB.h
+ *
+ *  Created on: Jul 8, 2024
+ *      Author: Jack Herron
+ */
+
+#ifndef CORE_INC_USB_HOST_URB_H_
+#define CORE_INC_USB_HOST_URB_H_
+
+
+
+
+
+#endif /* CORE_INC_USB_HOST_URB_H_ */
---
+++ USB Device And Host Development/USB_HOST/Core/Src/USB_Host_URB.c
@@ -0,0 +1,8 @@
+/*
+ * USB_Host_URB.c
+ *
+ *  Created on: Jul 8, 2024
+ *      Author: Jack Herron
+ */
+
+
