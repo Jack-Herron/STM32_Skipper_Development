52624a8e65c29bd395ad9385104c36ea8aa63f24
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-07-08 15:25:33 -0700

    Created a URB typedef struct.

--- USB Device And Host Development/USB_HOST/Core/Inc/USB_Host_URB.h
+++ USB Device And Host Development/USB_HOST/Core/Inc/USB_Host_URB.h
@@ -8,7 +8,16 @@
 #ifndef CORE_INC_USB_HOST_URB_H_
 #define CORE_INC_USB_HOST_URB_H_
 
+#include <stdint.h>
 
+typedef struct{
+	uint8_t 							port_Number;
+	uint8_t 							transfer_Direction;
+	uint8_t 							device_Address;
+	uint8_t								transfer_Type;
+	uint32_t							transfer_Length;
+	uint8_t								control_Setup_Packet[8];
+}USB_Host_URB___URB;
 
 
 
