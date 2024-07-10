f5567e655fc965033ed4a2a6f21a2348d1c78b72
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-07-09 19:26:22 -0700

    added an enum for transfer stage, as well a busy flag.

--- USB Device And Host Development/USB_HOST/Core/Inc/USB_Host_Transfers.h
+++ USB Device And Host Development/USB_HOST/Core/Inc/USB_Host_Transfers.h
@@ -13,16 +13,25 @@
 
 #define USB_Host_Transfers___CONTROL_SETUP_PACKET_LENGTH 		8
 
+typedef enum
+{
+	URB_Transfer_Stage___SETUP,
+	URB_Transfer_Stage___DATA,
+	URB_Transfer_Stage___STATUS,
+	URB_Transfer_Stage___COMPLETE
+}USB_Host_Transfers___URB_Transfer_Stage_TypeDef;
+
 typedef struct{
-	uint16_t									URB_ID;
-	uint8_t										device_Address;
-	uint8_t 									transfer_Direction;
-	uint8_t										transfer_Stage;
-	uint8_t										transfer_Type;
-	uint32_t									transfer_Length;
-	uint8_t										control_Setup_Packet[USB_Host_Transfers___CONTROL_SETUP_PACKET_LENGTH];
-	uint8_t*									transfer_Buffer;
-	void(*URB_Callback)							(uint8_t, uint8_t);
+	uint16_t											URB_ID;
+	uint8_t												device_Address;
+	uint8_t 											transfer_Direction;
+	USB_Host_Transfers___URB_Transfer_Stage_TypeDef		transfer_Stage;
+	uint8_t 											busy;
+	uint8_t												transfer_Type;
+	uint32_t											transfer_Length;
+	uint8_t												control_Setup_Packet[USB_Host_Transfers___CONTROL_SETUP_PACKET_LENGTH];
+	uint8_t*											transfer_Buffer;
+	void(*URB_Callback)									(uint8_t, uint8_t);
 }USB_Host_Transfers___URB_TypeDef;
 
 #endif /* CORE_INC_USB_HOST_TRANSFERS_H_ */
