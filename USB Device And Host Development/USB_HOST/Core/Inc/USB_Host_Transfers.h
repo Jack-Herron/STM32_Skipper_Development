f4ac252c048dfcb82b659ae8bb789464aeecceea
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-07-11 16:01:35 -0700

    added URB callback parameters to a #define to make them easy to adjust

--- USB Device And Host Development/USB_HOST/Core/Inc/USB_Host_Transfers.h
+++ USB Device And Host Development/USB_HOST/Core/Inc/USB_Host_Transfers.h
@@ -11,6 +11,8 @@
 #include <stdint.h>
 #include "../Inc/USB_Host_Device_Manager.h"
 
+struct USB_Host_Transfers___URB;
+
 #define USB_Host_Transfers___CONTROL_SETUP_PACKET_LENGTH 	8
 #define USB_Host_Transfers___URB_STAGE_SETUP				0
 #define USB_Host_Transfers___URB_STAGE_DATA					1
@@ -23,18 +25,19 @@
 #define USB_Host_Transfers___URB_DIRECTION_IN				0
 #define USB_Host_Transfers___URB_DIRECTION_OUT				1
 #define USB_Host_Transfers___URB_FIFO_QUEUE_LENGTH			USB_Host_Config___MAX_USB_REQUEST_BLOCKS
+#define USB_Host_Transfers___URB_CALLBACK_PARAMETERS		uint8_t port_Number, struct USB_Host_Transfers___URB URB
 
-typedef struct
+typedef struct USB_Host_Transfers___URB
 {
-	uint8_t												device_Address;
-	uint8_t 											transfer_Direction;
-	uint8_t												transfer_Stage;
-	uint8_t 											busy;
-	uint8_t												transfer_Type;
-	uint32_t											transfer_Length;
-	uint8_t												control_Setup_Packet[USB_Host_Transfers___CONTROL_SETUP_PACKET_LENGTH];
-	uint8_t*											transfer_Buffer;
-	void(*URB_Callback)									(uint8_t, uint8_t);
+	uint8_t											device_Address;
+	uint8_t 										transfer_Direction;
+	uint8_t											transfer_Stage;
+	uint8_t 										busy;
+	uint8_t											transfer_Type;
+	uint32_t										transfer_Length;
+	uint8_t											control_Setup_Packet[USB_Host_Transfers___CONTROL_SETUP_PACKET_LENGTH];
+	uint8_t*										transfer_Buffer;
+	void(*URB_Callback)								(USB_Host_Transfers___URB_CALLBACK_PARAMETERS);
 }USB_Host_Transfers___URB_TypeDef;
 
 typedef struct USB_Host_Transfers___URB_Node
@@ -46,8 +49,8 @@ typedef struct USB_Host_Transfers___URB_Node
 
 typedef struct
 {
-	USB_Host_Transfers___URB_Node_TypeDef* 		current_Node;
-	USB_Host_Transfers___URB_Node_TypeDef*		last_Node;
+	USB_Host_Transfers___URB_Node_TypeDef* 			current_Node;
+	USB_Host_Transfers___URB_Node_TypeDef*			last_Node;
 }USB_Host_Transfers___URB_Queue_TypeDef;
 
 #endif /* CORE_INC_USB_HOST_TRANSFERS_H_ */
