4a79052d6a5e657ba932905961baaa773d8867fc
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-07-13 23:23:40 -0700

    Switched device setup stages from device manager to enumerate header.

--- USB Device And Host Development/USB_HOST/Core/Inc/USB_Host_Device_Manager.h
+++ USB Device And Host Development/USB_HOST/Core/Inc/USB_Host_Device_Manager.h
@@ -25,14 +25,6 @@
 #define USB_Host_Device_Manager___FULL_SPEED_DEVICE									1
 #define USB_Host_Device_Manager___HIGH_SPEED_DEVICE									2
 
-#define USB_Host_Device_Manager___SETUP_STAGE_GET_FIRST_EIGHT_DEVICE_DESCRIPTOR 	0
-#define USB_Host_Device_Manager___SETUP_STAGE_SET_ADDRESS 							1
-#define USB_Host_Device_Manager___SETUP_STAGE_GET_FULL_DEVICE_DESCRIPTOR			2
-#define USB_Host_Device_Manager___SETUP_STAGE_GET_SHORT_STRING_DESCRIPTOR 			3
-#define USB_Host_Device_Manager___SETUP_STAGE_GET_FULL_STRING_DESCRIPTOR 			4
-#define USB_Host_Device_Manager___SETUP_STAGE_GET_SHORT_CONFIGURATION_DESCRIPTOR 	5
-#define USB_Host_Device_Manager___SETUP_STAGE_GET_FULL_CONFIGURATION_DESCRIPTOR		6
-#define USB_Host_Device_Manager___SETUP_STAGE_COMPLETE	 							7
 // Structures
 
 typedef struct{
