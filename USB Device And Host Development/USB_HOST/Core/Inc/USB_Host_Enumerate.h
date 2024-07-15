db614cee376e78f28af5512d95947348595c745d
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-07-14 18:50:02 -0700

    Added declaration for enumerate device function.

--- USB Device And Host Development/USB_HOST/Core/Inc/USB_Host_Enumerate.h
+++ USB Device And Host Development/USB_HOST/Core/Inc/USB_Host_Enumerate.h
@@ -8,6 +8,8 @@
 #ifndef CORE_INC_USB_HOST_ENUMERATE_H_
 #define CORE_INC_USB_HOST_ENUMERATE_H_
 
+#include <stdint.h>
+
 #define USB_Host_Enumerate___DEFAULT_MAX_PACKET_SIZE							8
 #define USB_Host_Enumerate___SETUP_STAGE_IDLE									0
 #define USB_Host_Enumerate___SETUP_STAGE_GET_FIRST_EIGHT_DEVICE_DESCRIPTOR 		1
@@ -18,5 +20,7 @@
 #define USB_Host_Enumerate___SETUP_STAGE_GET_SHORT_CONFIGURATION_DESCRIPTOR 	6
 #define USB_Host_Enumerate___SETUP_STAGE_GET_FULL_CONFIGURATION_DESCRIPTOR		7
 #define USB_Host_Enumerate___SETUP_STAGE_COMPLETE	 							8
+
+void USB_Host_Enumerate___Enumerate_Device(uint8_t port_Number, uint8_t device_Address);
 
 #endif /* CORE_INC_USB_HOST_ENUMERATE_H_ */
