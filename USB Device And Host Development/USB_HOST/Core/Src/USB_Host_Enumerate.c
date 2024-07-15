91541bc8589139fb500ab82399007524bbb8f9a7
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-07-14 18:50:29 -0700

    Added functions for getting device descriptor.

--- USB Device And Host Development/USB_HOST/Core/Src/USB_Host_Enumerate.c
+++ USB Device And Host Development/USB_HOST/Core/Src/USB_Host_Enumerate.c
@@ -7,6 +7,34 @@
 
 #include "../Inc/USB_Host_Enumerate.h"
 #include "../Inc/USB_Host_Device_Manager.h"
+#include "../Inc/USB_Host_Transfers.h"
+
+void USB_Host_Enumerate___URB_Callback(USB_Host_Transfers___URB_CALLBACK_PARAMETERS)
+{
+
+}
+
+void USB_Host_Enumerate___Get_Full_Device_Descriptor(uint8_t port_Number, uint8_t device_Address)
+{
+	USB_Host_Transfers___Control_Setup_Packet setup_Packet;
+	setup_Packet.bmRequestType 	= USB_Host_Transfers___CONTROL_SETUP_PACKET_BMREQUESTTYPE_STANDARD_DEVICE_TO_HOST;
+	setup_Packet.bRequest 		= USB_Host_Transfers___CONTROL_SETUP_PACKET_BREQUEST_GET_DESCRIPTOR;
+	setup_Packet.wValue 		= 0x0100;
+	setup_Packet.wIndex 		= 0x0000;
+	setup_Packet.wLength 		= 0x0012;
+	USB_Host_Transfers___Control_Transfer_In(device_Address, setup_Packet, 0, 0, USB_Host_Enumerate___URB_Callback);
+}
+
+void USB_Host_Enumerate___Get_First_Eight_Device_Descriptor(uint8_t port_Number, uint8_t device_Address)
+{
+	USB_Host_Transfers___Control_Setup_Packet setup_Packet;
+	setup_Packet.bmRequestType 	= USB_Host_Transfers___CONTROL_SETUP_PACKET_BMREQUESTTYPE_STANDARD_DEVICE_TO_HOST;
+	setup_Packet.bRequest 		= USB_Host_Transfers___CONTROL_SETUP_PACKET_BREQUEST_GET_DESCRIPTOR;
+	setup_Packet.wValue 		= 0x0100;
+	setup_Packet.wIndex 		= 0x0000;
+	setup_Packet.wLength 		= 0x08;
+	USB_Host_Transfers___Control_Transfer_In(device_Address, setup_Packet, 0, 0, USB_Host_Enumerate___URB_Callback);
+}
 
 void USB_Host_Enumerate___Do_Setup_Stage(uint8_t port_Number, uint8_t device_Address, uint8_t setup_Stage)
 {
@@ -15,27 +43,35 @@ void USB_Host_Enumerate___Do_Setup_Stage(uint8_t port_Number, uint8_t device_Add
 	case USB_Host_Enumerate___SETUP_STAGE_IDLE:
 
 		break;
-	case USB_Host_Enumerate___SETUP_STAGE_GET_FIRST_EIGHT_DEVICE_DESCRIPTOR:
 
+	case USB_Host_Enumerate___SETUP_STAGE_GET_FIRST_EIGHT_DEVICE_DESCRIPTOR:
+		USB_Host_Enumerate___Get_First_Eight_Device_Descriptor(port_Number, device_Address);
 		break;
+
 	case USB_Host_Enumerate___SETUP_STAGE_SET_ADDRESS:
 
 		break;
+
 	case USB_Host_Enumerate___SETUP_STAGE_GET_FULL_DEVICE_DESCRIPTOR:
 
 		break;
+
 	case USB_Host_Enumerate___SETUP_STAGE_GET_SHORT_STRING_DESCRIPTOR:
 
 		break;
+
 	case USB_Host_Enumerate___SETUP_STAGE_GET_FULL_STRING_DESCRIPTOR:
 
 		break;
+
 	case USB_Host_Enumerate___SETUP_STAGE_GET_SHORT_CONFIGURATION_DESCRIPTOR:
 
 		break;
+
 	case USB_Host_Enumerate___SETUP_STAGE_GET_FULL_CONFIGURATION_DESCRIPTOR:
 
 		break;
+
 	case USB_Host_Enumerate___SETUP_STAGE_COMPLETE:
 
 		break;
