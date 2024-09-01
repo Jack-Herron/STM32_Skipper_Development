3772e3d13cbbae8d644954983a0a22fc0fde7c80
Jack Herron <Jack Herron@DESKTOP-OVVPN70>
2024-08-31 20:00:47 -0700

    Finished hub setup process.

--- USB Device And Host Development/USB_HOST/Core/Inc/USB_Host_Hub.h
+++ USB Device And Host Development/USB_HOST/Core/Inc/USB_Host_Hub.h
@@ -16,11 +16,67 @@
 #define USB_Host_Hub___HUB_DEVICE_CLASS								0x09
 #define USB_Host_Hub___NUMBER_OF_BITS_IN_BYTE						(0x08)
 #define USB_Host_Hub___HUB_PORT_MASK_SIZE_IN_BYTES					USB_Host_Hub___QUICK_DIV_ROOF(USB_Host_Config___MAX_HUB_PORTS, USB_Host_Hub___NUMBER_OF_BITS_IN_BYTE)
+#define USB_Host_Hub___HUB_DESCRIPTOR_TYPE							0x29
+#define USB_Host_Hub___HUB_DESCRIPTOR_BASE_LENGTH					0x07
 
 #define USB_Host_Hub___HUB_SETUP_STAGE_SET_CONFIGURATION            0x00
 #define USB_Host_Hub___HUB_SETUP_STAGE_GET_SHORT_HUB_DESCRIPTOR     0x01
 #define USB_Host_Hub___HUB_SETUP_STAGE_GET_FULL_HUB_DESCRIPTOR      0x02
-#define USB_Host_Hub___HUB_SETUP_STAGE_ENABLE_PORT					0x03
+#define USB_Host_Hub___HUB_SETUP_STAGE_ENABLE_PORTS					0x03
+#define USB_Host_Hub___HUB_SETUP_STAGE_COMPLETE						0x04
+
+#define USB_Host_Hub___wPortChange_C_PORT_CONNECTION_Pos			(0x00)
+#define USB_Host_Hub___wPortChange_C_PORT_ENABLE_Pos				(0x01)
+#define USB_Host_Hub___wPortChange_C_PORT_SUSPEND_Pos				(0x02)
+#define USB_Host_Hub___wPortChange_C_PORT_OVER_CURRENT_Pos			(0x03)
+#define USB_Host_Hub___wPortChange_C_PORT_RESET_Pos				 	(0x04)
+
+#define USB_Host_Hub___wPortChange_C_PORT_CONNECTION_Msk			(1 << USB_Host_Hub___wPortChange_C_PORT_CONNECTION_Pos)
+#define USB_Host_Hub___wPortChange_C_PORT_ENABLE_Msk				(1 << USB_Host_Hub___wPortChange_C_PORT_ENABLE_Pos)
+#define USB_Host_Hub___wPortChange_C_PORT_SUSPEND_Msk				(1 << USB_Host_Hub___wPortChange_C_PORT_SUSPEND_Pos)
+#define USB_Host_Hub___wPortChange_C_PORT_OVER_CURRENT_Msk			(1 << USB_Host_Hub___wPortChange_C_PORT_OVER_CURRENT_Pos)
+#define USB_Host_Hub___wPortChange_C_PORT_RESET_Msk				 	(1 << USB_Host_Hub___wPortChange_C_PORT_RESET_Pos)
+
+#define USB_Host_Hub___wPortStatus_PORT_CONNECTION_Pos				(0x00)
+#define USB_Host_Hub___wPortStatus_PORT_ENABLE_Pos					(0x01)
+#define USB_Host_Hub___wPortStatus_PORT_SUSPEND_Pos					(0x02)
+#define USB_Host_Hub___wPortStatus_PORT_OVER_CURRENT_Pos			(0x03)
+#define USB_Host_Hub___wPortStatus_PORT_RESET_Pos				 	(0x04)
+#define USB_Host_Hub___wPortStatus_PORT_POWER_Pos				 	(0x08)
+#define USB_Host_Hub___wPortStatus_PORT_LOW_SPEED_Pos				(0x09)
+#define USB_Host_Hub___wPortStatus_PORT_HIGH_SPEED_Pos		 		(0x0a)
+#define USB_Host_Hub___wPortStatus_PORT_TEST_Pos				 	(0x0b)
+#define USB_Host_Hub___wPortStatus_PORT_INDICATOR_Pos			 	(0x0c)
+
+#define USB_Host_Hub___wPortStatus_PORT_CONNECTION_Msk				(1 << USB_Host_Hub___wPortStatus_PORT_CONNECTION_Pos)
+#define USB_Host_Hub___wPortStatus_PORT_ENABLE_Msk					(1 << USB_Host_Hub___wPortStatus_PORT_ENABLE_Pos)
+#define USB_Host_Hub___wPortStatus_PORT_SUSPEND_Msk					(1 << USB_Host_Hub___wPortStatus_PORT_SUSPEND_Pos)
+#define USB_Host_Hub___wPortStatus_PORT_OVER_CURRENT_Msk			(1 << USB_Host_Hub___wPortStatus_PORT_OVER_CURRENT_Pos)
+#define USB_Host_Hub___wPortStatus_PORT_RESET_Msk				 	(1 << USB_Host_Hub___wPortStatus_PORT_RESET_Pos)
+#define USB_Host_Hub___wPortStatus_PORT_POWER_Msk				 	(1 << USB_Host_Hub___wPortStatus_PORT_POWER_Pos)
+#define USB_Host_Hub___wPortStatus_PORT_LOW_SPEED_Msk				(1 << USB_Host_Hub___wPortStatus_PORT_LOW_SPEED_Pos)
+#define USB_Host_Hub___wPortStatus_PORT_HIGH_SPEED_Msk		 		(1 << USB_Host_Hub___wPortStatus_PORT_HIGH_SPEED_Pos)
+#define USB_Host_Hub___wPortStatus_PORT_TEST_Msk				 	(1 << USB_Host_Hub___wPortStatus_PORT_TEST_Pos)
+#define USB_Host_Hub___wPortStatus_PORT_INDICATOR_Msk			 	(1 << USB_Host_Hub___wPortStatus_PORT_INDICATOR_Pos)
+
+#define USB_Host_Hub___FEATURE_C_HUB_LOCAL_POWER	 				(0x00)
+#define USB_Host_Hub___FEATURE_C_HUB_OVER_CURRENT		 			(0x01)
+#define USB_Host_Hub___FEATURE_PORT_CONNECTION	 				 	(0x00)
+#define USB_Host_Hub___FEATURE_PORT_ENABLE		 				 	(0x01)
+#define USB_Host_Hub___FEATURE_PORT_SUSPEND		 				 	(0x02)
+#define USB_Host_Hub___FEATURE_PORT_OVER_CURRENT	 				(0x03)
+#define USB_Host_Hub___FEATURE_PORT_RESET			 				(0x04)
+#define USB_Host_Hub___FEATURE_PORT_POWER			 				(0x08)
+#define USB_Host_Hub___FEATURE_PORT_LOW_SPEED		 				(0x09)
+#define USB_Host_Hub___FEATURE_C_PORT_CONNECTION	 				(0x10)
+#define USB_Host_Hub___FEATURE_C_PORT_ENABLE		 				(0x11)
+#define USB_Host_Hub___FEATURE_C_PORT_SUSPEND	 				 	(0x12)
+#define USB_Host_Hub___FEATURE_C_PORT_OVER_CURRENT			 	 	(0x13)
+#define USB_Host_Hub___FEATURE_C_PORT_RESET	 				 	 	(0x14)
+#define USB_Host_Hub___FEATURE_PORT_TEST		 				 	(0x15)
+#define USB_Host_Hub___FEATURE_PORT_INDICATOR	 				 	(0x16)
+
+#define USB_Host_Hub___STANDARD_NUMBER_OF_RETRIES					0xffff
 
 void USB_Host_Hub___Initiate_Hub(uint8_t port_Number, uint8_t device_Address);
 
@@ -36,10 +92,9 @@ typedef struct __attribute__((__packed__)){
 
 typedef struct{
 	uint8_t                                 port_Number;
-	uint8_t 								address;
+	uint8_t 								device_Address;
 	USB_Host_Hub___Hub_Descriptor_TypeDef 	descriptor;
-	uint8_t 								hub_Setup_Stage;
-
+	uint8_t 								setup_Stage;
 }USB_Host_Hub___Hub_TypeDef;
 
 typedef struct USB_Host_Hub___Hub_Node
--- USB Device And Host Development/USB_HOST/Core/Src/USB_Host_Hub.c
+++ USB Device And Host Development/USB_HOST/Core/Src/USB_Host_Hub.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include "../Inc/USB_Host_Hub.h"
 #include "../Inc/USB_Host_Device_Manager.h"
+#include "../Inc/USB_Host_Transfers.h"
 
 #if USB_Host_Config___DYNAMICALLY_ALLOCATE_USB_HUBS == true
 
@@ -96,18 +97,143 @@ void USB_Host_Hub___Delete_Hub_Node(uint8_t port_Number, USB_Host_Hub___Hub_Node
 	}
 }
 
+USB_Host_Hub___Hub_Node_TypeDef* USB_Host_Hub___Get_Hub_Node_From_Device_Address(uint8_t port_Number, uint8_t device_Address)
+{
+	if (USB_Host_Hub___Hub_Queue[port_Number].first_Node != NULL)
+	{
+		USB_Host_Hub___Hub_Node_TypeDef* p_Hub_Node = USB_Host_Hub___Hub_Queue[port_Number].first_Node;
+		while (p_Hub_Node != NULL)
+		{
+			if (p_Hub_Node->hub.device_Address == device_Address)
+			{
+				return (p_Hub_Node);
+			}
+			p_Hub_Node = p_Hub_Node->next_Node;
+		}
+	}
+	return(NULL);
+}
+
+void USB_Host_Hub___URB_Callback(USB_Host_Transfers___URB_CALLBACK_PARAMETERS);
+
+void USB_Host_Hub___Set_Configuration(uint8_t port_Number, uint8_t device_Address, uint8_t configuration_Index)
+{
+	USB_Host_Transfers___Control_Setup_Packet setup_Packet;
+
+	setup_Packet.bmRequestType 	= USB_Host_Transfers___CONTROL_SETUP_PACKET_BMREQUESTTYPE_STANDARD_HOST_TO_DEVICE;
+	setup_Packet.bRequest 		= USB_Host_Transfers___CONTROL_SETUP_PACKET_BREQUEST_SET_CONFIGURATION;
+	setup_Packet.wValue 		= configuration_Index;
+	setup_Packet.wIndex 		= 0;
+	setup_Packet.wLength 		= 0;
+
+	USB_Host_Transfers___Control_Transfer(port_Number, device_Address, USB_Host___ENDPOINT_ZERO, USB_Host___TRANSFER_DIRECTION_OUT, setup_Packet, 0, 0, USB_Host_Hub___STANDARD_NUMBER_OF_RETRIES, USB_Host_Hub___URB_Callback);
+}
+
+void USB_Host_Hub___Get_Hub_Descriptor(uint8_t port_Number, uint8_t device_Address, uint8_t* p_Buffer, uint8_t descriptor_Length)
+{
+	USB_Host_Transfers___Control_Setup_Packet setup_Packet;
+
+	setup_Packet.bmRequestType 	= USB_Host_Transfers___CONTROL_SETUP_PACKET_BMREQUESTTYPE_CLASS_DEVICE_TO_HOST;
+	setup_Packet.bRequest 		= USB_Host_Transfers___CONTROL_SETUP_PACKET_BREQUEST_GET_DESCRIPTOR;
+	setup_Packet.wValue 		= (USB_Host_Hub___HUB_DESCRIPTOR_TYPE << 8);
+	setup_Packet.wIndex 		= 0;
+	setup_Packet.wLength 		= descriptor_Length;
+
+	USB_Host_Transfers___Control_Transfer(port_Number, device_Address, USB_Host___ENDPOINT_ZERO, USB_Host___TRANSFER_DIRECTION_IN, setup_Packet, p_Buffer, descriptor_Length, USB_Host_Hub___STANDARD_NUMBER_OF_RETRIES, USB_Host_Hub___URB_Callback);
+}
+
+void USB_Host_Hub___Set_Port_Feature(uint8_t port_Number, uint8_t device_Address, uint8_t hub_Port_Number, uint8_t main_Feature_Selector, uint8_t sub_Feature_Selector)
+{
+	USB_Host_Transfers___Control_Setup_Packet setup_Packet;
+
+	setup_Packet.bmRequestType 	= USB_Host_Transfers___CONTROL_SETUP_PACKET_BMREQUESTTYPE_CLASS_HOST_TO_OTHER;
+	setup_Packet.bRequest 		= USB_Host_Transfers___CONTROL_SETUP_PACKET_BREQUEST_SET_FEATURE;
+	setup_Packet.wValue 		= main_Feature_Selector;
+	setup_Packet.wIndex 		= hub_Port_Number | (sub_Feature_Selector << 8);
+	setup_Packet.wLength 		= 0;
+
+	USB_Host_Transfers___Control_Transfer(port_Number, device_Address, USB_Host___ENDPOINT_ZERO, USB_Host___TRANSFER_DIRECTION_OUT, setup_Packet, 0, 0, USB_Host_Hub___STANDARD_NUMBER_OF_RETRIES, USB_Host_Hub___URB_Callback);
+}
+
+void USB_Host_Hub___Setup_Stage_Completed(USB_Host_Hub___Hub_Node_TypeDef* p_USB_Hub_Node)
+{
+	switch(p_USB_Hub_Node->hub.setup_Stage)
+	{
+	case USB_Host_Hub___HUB_SETUP_STAGE_SET_CONFIGURATION:
+
+		break;
+	case USB_Host_Hub___HUB_SETUP_STAGE_GET_SHORT_HUB_DESCRIPTOR:
+
+		break;
+	case USB_Host_Hub___HUB_SETUP_STAGE_GET_FULL_HUB_DESCRIPTOR:
+
+		break;
+	case USB_Host_Hub___HUB_SETUP_STAGE_ENABLE_PORTS:
+
+		break;
+	}
+}
+
+void USB_Host_Hub___Set_Next_Setup_Stage(USB_Host_Hub___Hub_Node_TypeDef* p_USB_Hub_Node)
+{
+	switch(p_USB_Hub_Node->hub.setup_Stage)
+	{
+	case USB_Host_Hub___HUB_SETUP_STAGE_SET_CONFIGURATION:
+		p_USB_Hub_Node->hub.setup_Stage = USB_Host_Hub___HUB_SETUP_STAGE_GET_SHORT_HUB_DESCRIPTOR;
+		break;
+	case USB_Host_Hub___HUB_SETUP_STAGE_GET_SHORT_HUB_DESCRIPTOR:
+		p_USB_Hub_Node->hub.setup_Stage = USB_Host_Hub___HUB_SETUP_STAGE_GET_FULL_HUB_DESCRIPTOR;
+		break;
+	case USB_Host_Hub___HUB_SETUP_STAGE_GET_FULL_HUB_DESCRIPTOR:
+		p_USB_Hub_Node->hub.setup_Stage = USB_Host_Hub___HUB_SETUP_STAGE_ENABLE_PORTS;
+		break;
+	case USB_Host_Hub___HUB_SETUP_STAGE_ENABLE_PORTS:
+		p_USB_Hub_Node->hub.setup_Stage = USB_Host_Hub___HUB_SETUP_STAGE_COMPLETE;
+		break;
+	}
+}
+
 void USB_Host_Hub___Do_Setup_Stage(USB_Host_Hub___Hub_Node_TypeDef* p_USB_Hub_Node)
 {
+	switch(p_USB_Hub_Node->hub.setup_Stage)
+	{
+	case USB_Host_Hub___HUB_SETUP_STAGE_SET_CONFIGURATION:
+		USB_Host_Hub___Set_Configuration(p_USB_Hub_Node->hub.port_Number, p_USB_Hub_Node->hub.device_Address, 0);
+		break;
+	case USB_Host_Hub___HUB_SETUP_STAGE_GET_SHORT_HUB_DESCRIPTOR:
+		USB_Host_Hub___Get_Hub_Descriptor(p_USB_Hub_Node->hub.port_Number, p_USB_Hub_Node->hub.device_Address, (uint8_t*)&(p_USB_Hub_Node->hub.descriptor), USB_Host_Hub___HUB_DESCRIPTOR_BASE_LENGTH);
+		break;
+	case USB_Host_Hub___HUB_SETUP_STAGE_GET_FULL_HUB_DESCRIPTOR:
+		USB_Host_Hub___Get_Hub_Descriptor(p_USB_Hub_Node->hub.port_Number, p_USB_Hub_Node->hub.device_Address, (uint8_t*)&(p_USB_Hub_Node->hub.descriptor), p_USB_Hub_Node->hub.descriptor.bDescriptorLength);
+		break;
+	case USB_Host_Hub___HUB_SETUP_STAGE_ENABLE_PORTS:
+		for(uint8_t i = 0; i < p_USB_Hub_Node->hub.descriptor.bNumberOfPorts; i++)
+		{
+			USB_Host_Hub___Set_Port_Feature(p_USB_Hub_Node->hub.port_Number, p_USB_Hub_Node->hub.device_Address, i+1, USB_Host_Hub___FEATURE_PORT_POWER, 0);
+		}
+		break;
+	}
+}
 
+void USB_Host_Hub___URB_Callback(USB_Host_Transfers___URB_CALLBACK_PARAMETERS)
+{
+	USB_Host_Hub___Hub_Node_TypeDef* p_Hub_Node = USB_Host_Hub___Get_Hub_Node_From_Device_Address(URB.port_Number, URB.device_Address);
+
+	if(p_Hub_Node != NULL)
+	{
+		USB_Host_Hub___Setup_Stage_Completed(p_Hub_Node);
+		USB_Host_Hub___Set_Next_Setup_Stage(p_Hub_Node);
+		USB_Host_Hub___Do_Setup_Stage(p_Hub_Node);
+	}
 }
 
 void USB_Host_Hub___Initiate_Hub(uint8_t port_Number, uint8_t device_Address)
 {
-	USB_Host_Hub___Hub_Node_TypeDef* p_USB_Hub_Node = USB_Host_Hub___Allocate_Hub_Node();
+	USB_Host_Hub___Hub_Node_TypeDef* p_USB_Hub_Node = USB_Host_Hub___Create_Enumerator(port_Number);
 	if(p_USB_Hub_Node != NULL)
 	{
-		p_USB_Hub_Node->hub.hub_Setup_Stage = USB_Host_Hub___HUB_SETUP_STAGE_SET_CONFIGURATION;
-		p_USB_Hub_Node->hub.address 		= device_Address;
+		p_USB_Hub_Node->hub.setup_Stage = USB_Host_Hub___HUB_SETUP_STAGE_SET_CONFIGURATION;
+		p_USB_Hub_Node->hub.device_Address 		= device_Address;
 		p_USB_Hub_Node->hub.port_Number 	= port_Number;
 		USB_Host_Hub___Do_Setup_Stage(p_USB_Hub_Node);
 	}
