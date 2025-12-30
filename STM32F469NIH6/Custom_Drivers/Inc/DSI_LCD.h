b4f2732b8ee0986fcb69750e6f4439a793e3e296
Jack Herron <jackherron671@gmail.com>
2025-12-30 01:54:33 -0800

    DSI Working, pattern generator enabled.

--- STM32F469NIH6/Custom_Drivers/Inc/DSI_LCD.h
+++ STM32F469NIH6/Custom_Drivers/Inc/DSI_LCD.h
@@ -8,30 +8,49 @@
 #ifndef INC_DSI_LCD_H_
 #define INC_DSI_LCD_H_
 
+// -- configuration values --
 
+#define DSI_LCD___HACT					800	// Display horizontal active size 		(pixels)
+#define DSI_LCD___HSA					2	// Display horizontal sync size 		(pixels)
+#define DSI_LCD___HFP					34	// Display horizontal front porch size 	(pixels)
+#define DSI_LCD___HBP					34	// Display horizontal back porch size 	(pixels)
+#define DSI_LCD___VACT					480	// Display vertical actual size 		(pixels)
+#define DSI_LCD___VSA					120	// Display vertical sync size 			(pixels)
+#define DSI_LCD___VFP					150	// Display vertical front porch size 	(pixels)
+#define DSI_LCD___VBP					150	// Display vertical back porch size 	(pixels)
 
-#define DSI_LCD___LANE_PERIODX4			8 	// Lane period in 0.25ns increments (for 500Mbps: LANEPERIODX4 = (period[ns] * 4) = (2ns*4) = 8)
-#define DSI_LCD___LANE_COUNT			2 	// number of data lanes, 1: 1 lane, 2: 2 lanes
-#define DSI_LCD___DATA_HS2LP_TIME		15	// Max high speed to low power mode switch time in lengths of byte lane periods		 One byte-cycle period = 8 * UI, where UI = 1 / lane_bitrate (seconds per bit)
-#define DSI_LCD___CLOCK_HS2LP_TIME 		28	// same as data HS2LP time, but for the clock lane
-#define DSI_LCD___DATA_LP2HS_TIME		25	// same idea
-#define DSI_LCD___CLOCK_LP2HS_TIME		33	// same idea
-#define DSI_LCD___STOP_WAIT_TIME		10 	// the minimum wait period to request a high-speed transmission after the Stop state
-#define DSI_LCD___TX_ESCAPE_CLOCK_DIV	4	// indicates the division factor for the TX escape clock source (lanebyteclk)
-#define DSI_LCD___TX_TIMEOUT_CLOCK_DIV	1	// indicates the division factor for the timeout clock
+#define DSI_LDC___LANE_FREQUENCY		500000000
 
-#define DSI_LCD___DATA_TYPE_LONG_WRITE		0x39	// DCS mode for long packet write
-#define DSI_LCD___DATA_TYPE_SHORT_WRITE_1P	0x15	// DCS mode for short packet write with one parameter
-#define DSI_LCD___DATA_TYPE_SHORT_WRITE_0P	0x05	// DCS mode for short packet write with one parameter
-#define DSI_LCD___PIXEL_FORMAT_RGB888		0x77 	// DCS code for RGB888
-#define DSI_LCD___CMD_SLEEPOUT				0x11
-#define DSI_LCD___CMD_SET_PIXEL_FORMAT		0x3A	// DCS Register to set the color format
-#define DSI_LCD___CMD_SET_MADCTR			0x36	// Register for MADCTR
-#define DSI_LCD___MADCTR_A0					0xA0
-#define DSI_LCD___CMD_DISPLAY_ON			0x29
-#define DSI_LCD___CMD_CABC_SET_MODE			0x55
-#define DSI_LCD___CABC_MODE_STILL_PICTURE	0x02
-#define DSI_LCD___CMD_ALLPON				0x23
+#define DSI_LCD___LANE_COUNT				0x02 	// number of data lanes, 1: 1 lane, 2: 2 lanes
+#define DSI_LCD___DATA_HS2LP_TIME			0x23	// Max high speed to low power mode switch time in lengths of byte lane periods		 One byte-cycle period = 8 * UI, where UI = 1 / lane_bitrate (seconds per bit)
+#define DSI_LCD___CLOCK_HS2LP_TIME 			0x23	// same as data HS2LP time, but for the clock lane
+#define DSI_LCD___DATA_LP2HS_TIME			0x23	// same idea
+#define DSI_LCD___CLOCK_LP2HS_TIME			0x23	// same idea
+#define DSI_LCD___DATA_MAXIMUM_READ_TIME	0x00	// Read timeout in lane byte periods
+
+#define DSI_LCD___STOP_WAIT_TIME			0x0a 	// the minimum wait period to request a high-speed transmission after the Stop state
+#define DSI_LCD___TX_ESCAPE_CLOCK_DIV		4		// indicates the division factor for the TX escape clock source (lanebyteclk)
+#define DSI_LCD___TX_TIMEOUT_CLOCK_DIV		0		// indicates the division factor for the timeout clock
+
+// -- Calculated values --
+
+#define DSI_LCD___LANE_BYTE_CLK				DSI_LDC___LANE_FREQUENCY / 8
+#define DSI_LCD___LANE_PERIODX4				(uint32_t)((1.0/DSI_LDC___LANE_FREQUENCY)*1e9*4) // Lane period in 0.25ns increments
+
+#define DSI_LCD___HLINE						(uint32_t)((DSI_LCD___HACT + DSI_LCD___HSA + DSI_LCD___HBP + DSI_LCD___HFP) * ((uint64_t)DSI_LCD___LANE_BYTE_CLK) / ((uint64_t)LCDTFT_FREQ))
+#define DSI_LCD___HSA_FOR_DSI				(uint32_t)((DSI_LCD___HSA) * ((uint64_t)DSI_LCD___LANE_BYTE_CLK) / ((uint64_t)LCDTFT_FREQ))
+#define DSI_LCD___HBP_FOR_DSI				(uint32_t)((DSI_LCD___HBP) * ((uint64_t)DSI_LCD___LANE_BYTE_CLK) / ((uint64_t)LCDTFT_FREQ))
+
+#define DSI_LCD___DATA_TYPE_SHORT_WRITE_1P 	0x15
+#define DSI_LCD___DATA_TYPE_LONG_WRITE		0x39
+
+// Register Fill values
+
+#define DSI_LCD___LCOLC_RGB888 				0x05
+#define DSI_LCD___COLMUX_RGB888				0x0A
+#define DSI_LCD___VMT_MODE_BURST			0x02
+
+// Functions
 
 void DSI_LCD___Long_Write(uint32_t channel_ID, uint32_t data_Type, uint8_t *pdata, uint32_t size);
 void DSI_LCD___Short_Write(uint32_t channel_ID, uint32_t data_Type, uint32_t data1, uint32_t data2);
--- STM32F469NIH6/Custom_Drivers/Src/DSI_LCD.c
+++ STM32F469NIH6/Custom_Drivers/Src/DSI_LCD.c
@@ -8,26 +8,23 @@
 #include "stm32f4xx.h"
 #include "DSI_LCD.h"
 #include "Clock.h"
-#include "otm8009a.h"
+#include "nt35510.h"
 
 void DSI_IO_WriteCmd(uint32_t NbrParams, uint8_t *pParams)
 {
-	switch(NbrParams)
+	if(NbrParams <= 1)
 	{
-	case 0:
 		DSI_LCD___Short_Write(0, DSI_LCD___DATA_TYPE_SHORT_WRITE_1P, pParams[0], pParams[1]);
-		break;
-	/*case 1:
-		DSI_LCD___Short_Write(0, DSI_LCD___DATA_TYPE_SHORT_WRITE_1P, pParams[0], pParams[1]);
-		break;*/
-	default:
+	}
+	else
+	{
 		DSI_LCD___Long_Write(0, DSI_LCD___DATA_TYPE_LONG_WRITE, pParams, NbrParams+1);
 	}
 }
 
-void OTM8009A_IO_Delay(uint32_t Delay)
+void NT35510_IO_Delay(uint32_t Delay)
 {
-	for(uint32_t i = 0; i < (SystemCoreClock / 1000) * Delay; i++);		// crude delay
+	for(uint32_t i = 0; i < (SystemCoreClock / 10000) * Delay; i++);		// crude delay
 }
 
 void DSI_LCD___Short_Write(uint32_t channel_ID, uint32_t data_Type, uint32_t data1, uint32_t data2)
@@ -36,30 +33,38 @@ void DSI_LCD___Short_Write(uint32_t channel_ID, uint32_t data_Type, uint32_t dat
 	DSI->GHCR = (data_Type | (channel_ID << DSI_GHCR_VCID_Pos) | (data1 << DSI_GHCR_WCLSB_Pos) | (data2 << DSI_GHCR_WCMSB_Pos));
 }
 
+//DSI_LCD___Long_Write takes a list of parameters, with the last byte being the command byte [p1, p2, ..., pn, cmd]
 void DSI_LCD___Long_Write(uint32_t channel_ID, uint32_t data_Type, uint8_t *pdata, uint32_t size)
 {
-	uint8_t cmd = pdata[0];
-	uint32_t payload_len = size; // include command byte
+	uint8_t command = pdata[size - 1];
 
 	while (!(DSI->GPSR & DSI_GPSR_CMDFE)); 									// Wait until command FIFO is empty
 
+	uint32_t first_Word = command;
+	uint32_t first_Word_Params = (size <= 4) ? (size) : (4);
 
-
-	for (uint32_t i = 0; i < payload_len; i += 4)
-	{
-		uint32_t b0 = (i + 0 < payload_len) ? ((i + 0 == 0) ? cmd : pdata[(i + 0)]) : 0;
-		uint32_t b1 = (i + 1 < payload_len) ? ((i + 1 == 0) ? cmd : pdata[(i + 1)]) : 0;
-		uint32_t b2 = (i + 2 < payload_len) ? ((i + 2 == 0) ? cmd : pdata[(i + 2)]) : 0;
-		uint32_t b3 = (i + 3 < payload_len) ? ((i + 3 == 0) ? cmd : pdata[(i + 3)]) : 0;
-
-		DSI->GPDR = (b0 << DSI_GPDR_DATA1_Pos)
-				  | (b1 << DSI_GPDR_DATA2_Pos)
-				  | (b2 << DSI_GPDR_DATA3_Pos)
-				  | (b3 << DSI_GPDR_DATA4_Pos);
+	for (uint32_t i = 1; i < first_Word_Params; i++) {
+		first_Word |= pdata[i-1] << (8 * (i));
 	}
 
-	uint32_t wc_lsb = (payload_len & 0xFF);
-	uint32_t wc_msb = ((payload_len >> 8) & 0xFF);
+	DSI->GPDR = first_Word;
+
+	uint32_t remaining_Size = size - first_Word_Params;
+
+	for (uint32_t i = 0; i < remaining_Size; i += 4) {
+		uint32_t word = 0;
+		uint32_t bytes_In_Word = (remaining_Size - i >= 4) ? 4 : (remaining_Size - i);
+
+		for (uint32_t j = 0; j < bytes_In_Word; j++)
+		{
+			word |= pdata[first_Word_Params + i + j - 1] << (8 * j);
+		}
+
+		DSI->GPDR = word;
+	}
+
+	uint32_t wc_lsb = (size & 0xFF);
+	uint32_t wc_msb = ((size >> 8) & 0xFF);
 
 	DSI->GHCR = (data_Type)
 	              | (channel_ID << DSI_GHCR_VCID_Pos)
@@ -69,68 +74,81 @@ void DSI_LCD___Long_Write(uint32_t channel_ID, uint32_t data_Type, uint8_t *pdat
 
 void DSI_LCD___Init(void)
 {
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOHEN; 											// Enable GPIOH clock
-	GPIOH->MODER |= GPIO_MODER_MODER7_0; 											// Set PH7 to Output mode
-	GPIOH->ODR &= ~GPIO_ODR_OD7; 													// Set PH7 Low (LCD Reset active)
-	for(uint32_t i = 0; i < (SystemCoreClock / 10); i++); 							// Delay 100ms
-	GPIOH->ODR |= GPIO_ODR_OD7; 													// Set PH7 High (LCD Reset inactive)
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOHEN; 												// Enable GPIOH clock
+	GPIOH->MODER |= GPIO_MODER_MODER7_0; 												// Set PH7 to Output mode
+	GPIOH->ODR &= ~GPIO_ODR_OD7; 														// Set PH7 Low (LCD Reset active)
+	for(uint32_t i = 0; i < (SystemCoreClock / 10); i++); 								// Delay 100ms
+	GPIOH->ODR |= GPIO_ODR_OD7; 														// Set PH7 High (LCD Reset inactive)
 
 	// Enable DSI clock
 	RCC->APB2ENR |= RCC_APB2ENR_DSIEN;
-	while (!(RCC->APB2ENR & RCC_APB2ENR_DSIEN)); 									// Wait for DSI clock to be enabled
-	DSI->MCR &= ~DSI_MCR_CMDM; // enable video mode
-	DSI->WRPCR = 0; 																// Reset DSI Wrapper
+	while (!(RCC->APB2ENR & RCC_APB2ENR_DSIEN)); 										// Wait for DSI clock to be enabled
+
+	DSI->WRPCR = 0; 																	// Reset DSI Wrapper
 
 	// DSI Regulator enable
-	DSI->WRPCR |= DSI_WRPCR_REGEN; 													// Enable DSI Wrapper Regulator
-	while(!(DSI->WISR & DSI_WISR_RRS)); 											// Wait for Regulator Ready Interrupt Flag
+	DSI->WRPCR |= DSI_WRPCR_REGEN; 														// Enable DSI Wrapper Regulator
+	while(!(DSI->WISR & DSI_WISR_RRS)); 												// Wait for Regulator Ready Interrupt Flag
 
 	// DSI PLL Configuration
-	DSI->WRPCR |= 	(PLLDSINDIV << 	DSI_WRPCR_PLL_NDIV_Pos) |						// Set PLL N Divider
-					(PLLDSIIDF 	<< 	DSI_WRPCR_PLL_IDF_Pos) 	|						// Set PLL IDF);
-					(PLLDSIODF 	<< 	DSI_WRPCR_PLL_ODF_Pos);							// Set PLL ODF
-    DSI->WRPCR |= DSI_WRPCR_PLLEN; 													// Enable DSI PLL
-    while(!(DSI->WISR & DSI_WISR_PLLLS)); 											// Wait for PLL Lock Interrupt Flag
+	DSI->WRPCR |= 	(PLLDSINDIV << 	DSI_WRPCR_PLL_NDIV_Pos) |							// Set PLL N Divider
+					(PLLDSIIDF 	<< 	DSI_WRPCR_PLL_IDF_Pos) 	|							// Set PLL IDF);
+					(PLLDSIODF 	<< 	DSI_WRPCR_PLL_ODF_Pos);								// Set PLL ODF
+    DSI->WRPCR |= DSI_WRPCR_PLLEN; 														// Enable DSI PLL
+    while(!(DSI->WISR & DSI_WISR_PLLLS)); 												// Wait for PLL Lock Interrupt Flag
 
     // DSI Host Configuration
-    DSI->WPCR[0] 	|= (DSI_LCD___LANE_PERIODX4 << DSI_WPCR0_UIX4_Pos); 			// Set HS to LP time
-    DSI->PCONFR 	|=((DSI_LCD___LANE_COUNT-1) << DSI_PCONFR_NL_Pos);				// Set number of lanes to 2
-    DSI->CLCR 		|= 	DSI_CLCR_DPCC; 												// Enable D-PHY Clock HS mode
-    DSI->CLCR 		&= ~DSI_CLCR_ACR; 												// Disable Automatic Clock control
-    DSI->CLTCR 		= 	DSI_LCD___CLOCK_HS2LP_TIME 	<< DSI_CLTCR_HS2LP_TIME_Pos |	// Set HS to LP time for clock lanes
-    					DSI_LCD___CLOCK_LP2HS_TIME 	<< DSI_CLTCR_LP2HS_TIME_Pos; 	// Set LP to HS time for clock lanes
-	DSI->DLTCR 		= 	DSI_LCD___DATA_HS2LP_TIME 	<< DSI_DLTCR_HS2LP_TIME_Pos |	// Set HS to LP time for data lanes
-						DSI_LCD___DATA_LP2HS_TIME 	<< DSI_DLTCR_LP2HS_TIME_Pos; 	// Set LP to HS time for data lanes
-	DSI->PCONFR 	|= 	DSI_LCD___STOP_WAIT_TIME 	<< DSI_PCONFR_SW_TIME_Pos;		// Set the minimum wait period to request a high-speed transmission after the Stop state.
-	DSI->CCR        =   DSI_LCD___TX_TIMEOUT_CLOCK_DIV << DSI_CCR_TOCKDIV_Pos	|	// Set the timeout clock division factor
-						DSI_LCD___TX_ESCAPE_CLOCK_DIV << DSI_CCR_TXECKDIV_Pos;		// Set the timeout clock division factor
-	DSI->VMCR       =   DSI_VMCR_LPCE | 											// Enable Low-power command enable
-						0;										        // Set Video mode Type to  sync pulse mode
+    DSI -> WPCR[0] 	=  	(DSI_LCD___LANE_PERIODX4 		<< DSI_WPCR0_UIX4_Pos); 		// Set HS to LP time
+    DSI -> PCONFR 	=	((DSI_LCD___LANE_COUNT-1) 		<< DSI_PCONFR_NL_Pos) 		|	// Set number of lanes to 2
+    					DSI_LCD___STOP_WAIT_TIME 		<< DSI_PCONFR_SW_TIME_Pos;		// Set the minimum wait period to request a high-speed transmission after the Stop state.
+    DSI -> CLCR 	= 	DSI_CLCR_DPCC; 													// Enable D-PHY Clock HS mode
+    DSI -> CLTCR 	= 	DSI_LCD___CLOCK_HS2LP_TIME 		<< DSI_CLTCR_HS2LP_TIME_Pos |	// Set HS to LP time for clock lanes
+    					DSI_LCD___CLOCK_LP2HS_TIME 		<< DSI_CLTCR_LP2HS_TIME_Pos; 	// Set LP to HS time for clock lanes
+	DSI -> DLTCR 	= 	DSI_LCD___DATA_HS2LP_TIME 		<< DSI_DLTCR_HS2LP_TIME_Pos |	// Set HS to LP time for data lanes
+						DSI_LCD___DATA_LP2HS_TIME 		<< DSI_DLTCR_LP2HS_TIME_Pos; 	// Set LP to HS time for data lanes
 
-	DSI->VPCR = 		800;								// set horizontal pixal count
-	DSI->VCCR = 		1	<<	DSI_VCCR_NUMC_Pos;			// set number of packets per line
-	DSI->VNPCR = 		0	 << DSI_VNPCR_NPSIZE_Pos;		// disables null packets
-	DSI->VLCR =         1575 << DSI_VLCR_HLINE_Pos;														//?????? Check value
-	DSI->VVSACR  = 		(4   << DSI_VVSACR_VSA_Pos);
-	DSI->VVBPCR = 		(14  << DSI_VVBPCR_VBP_Pos);
-	DSI->VVFPCR = 		(14  << DSI_VVFPCR_VFP_Pos);
-	DSI->VVACR = 		(480 << DSI_VVACR_VA_Pos);
+	DSI -> CCR      =   DSI_LCD___TX_TIMEOUT_CLOCK_DIV 	<< DSI_CCR_TOCKDIV_Pos	|		// Set the timeout clock division factor
+						DSI_LCD___TX_ESCAPE_CLOCK_DIV 	<< DSI_CCR_TXECKDIV_Pos;		// Set the timeout clock division factor
 
+	DSI -> PCTLR 	=   DSI_PCTLR_DEN	| 												// Enable DSI digital section
+						DSI_PCTLR_CKE; 													// Enable DSI clocks													// Enable DSI
 
-	//DSI->LCCCR |=       DSI_LCCCR_COLC0 | DSI_LCCCR_COLC2; 						// Enable Color Mode RGB888
-	DSI->LCOLCR |=      DSI_LCOLCR_COLC0 | 4; 										// Enable Color Mode RGB888
+	DSI -> WPCR[0]  =   DSI_LCD___LANE_PERIODX4;	 									// Set the clock to transmit at selected frequency
+	DSI -> VMCR     =   DSI_LCD___VMT_MODE_BURST | 										// Set Video mode to Burst
+						DSI_VMCR_LPCE 			 |										// Enable Low Power Command Enable
+						DSI_VMCR_LPVSAE          |
+						DSI_VMCR_LPVBPE          |
+						DSI_VMCR_LPVFPE          |
+						DSI_VMCR_LPVAE           |
+						DSI_VMCR_LPHBPE          |
+						DSI_VMCR_LPHFPE          |
+						DSI_VMCR_LPCE;
 
+	DSI -> LPMCR    =   0x10 << DSI_LPMCR_LPSIZE_Pos; 									// Set Low Power Size to 16 bytes
+	DSI -> PCR      =   DSI_PCR_BTAE; 													// Enable Bus Turn Around
 
+	DSI -> VPCR		=   DSI_LCD___HACT; 												// Set packet size to contain 1 line of pixel data
+	DSI -> VCCR		= 	0; 																// Set to zero because 1 line per packet
+	DSI -> VNPCR	=   0xfff; 															// Set null packet size (burst mode so max)
+	DSI -> VLCR	    =   DSI_LCD___HLINE; 												// Set the total number of pixel clock periods per line
+	DSI -> VHSACR	=   DSI_LCD___HSA_FOR_DSI; 											// Set Horizontal Synchronism Active duration
+    DSI -> VHBPCR	=   DSI_LCD___HBP_FOR_DSI; 											// Set Horizontal Back Porch duration
 
-	DSI->PCTLR |=       DSI_PCTLR_DEN; 												// Enable DSI digital section
-	DSI->PCTLR |=       DSI_PCTLR_CKE; 												// Enable DSI clocks
-	//DSI->MCR &= ~DSI_MCR_CMDM; // enable video mode
-	DSI->WCR 		|= DSI_WCR_DSIEN;												// Enable DSI Wrapper
-	DSI -> CR 		|= DSI_CR_EN;													// Enable DSI
+    DSI -> VVSACR   =   DSI_LCD___VSA; 													// Set Vertical Synchronism Active duration in pixels
+    DSI -> VVACR    =   DSI_LCD___VACT; 												// Set Vertical Active duration in pixels
+    DSI -> VVFPCR   =   DSI_LCD___VFP; 													// Set Vertical Front Porch duration in pixels
+    DSI -> VVBPCR   =   DSI_LCD___VBP; 													// Set Vertical Back Porch duration in pixels
 
-	DSI->MCR |= DSI_MCR_CMDM; // enable command mode
-	OTM8009A_Init(OTM8009A_FORMAT_RGB888, OTM8009A_ORIENTATION_LANDSCAPE);
-	DSI->MCR &= ~DSI_MCR_CMDM; // enable video mode
-	DSI->VMCR |=        DSI_VMCR_PGM;
-	DSI->VMCR |=        DSI_VMCR_PGE; 												// Enable Pattern Generator
+    DSI -> LCOLCR   =   DSI_LCD___LCOLC_RGB888; 										// Set color coding format to RGB888
+    DSI -> WCFGR    =   DSI_LCD___COLMUX_RGB888;
+
+    DSI -> WCR 		= 	DSI_WCR_DSIEN;										 			// Enable DSI Wrapper
+    DSI -> CR 		= 	DSI_CR_EN; 														// Enable DSI
+
+    NT35510_Init(NT35510_FORMAT_RGB888, NT35510_ORIENTATION_LANDSCAPE);
+
+    NT35510_IO_Delay(100);
+
+	DSI -> MCR &= ~DSI_MCR_CMDM;
+	DSI -> VMCR |= DSI_VMCR_PGE;  // enable pattern gen
 }
